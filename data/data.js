487a40bac400813487258c3b258e138366d96509
atemp678 <ajt2158@columbia.edu>
2022-01-02 00:09:23 +0000

    added movie trailers and movies to data list

--- data/data.js
+++ data/data.js
@@ -1,9 +1,34 @@
 const movies = [
-    { id: 1, title: "Harry Potter", author: "J.K. Rowling", URL:"https://www.youtube.com/watch?v=VyHV0BRtdxo" },
-    { id: 2, title: "Clean Code", author: "Robert Martin", URL:"" },
-    { id: 3, title: "The Night Circus", author: "Erin Morgenstern", URL:"" },
-    { id: 4, title: "Rivers of London", author: "Ben Aaronovich",  URL:""},
-    { id: 5, title: "Crime and Punishment", author: "Fyodr Dostoyevsky", URL:"" }
-  ];
-  
-  export default movies;
+  {
+    id: 1,
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    URL: "https://www.youtube.com/watch?v=VyHV0BRtdxo",
+  },
+  {
+    id: 2,
+    title: "Titanic",
+    author: "James Cameron",
+    URL: "https://www.youtube.com/watch?v=kVrqfYjkTdQ",
+  },
+  {
+    id: 3,
+    title: "Don't Look Up",
+    author: "Adam McKay",
+    URL: "https://www.youtube.com/watch?v=RbIxYm3mKzI",
+  },
+  {
+    id: 4,
+    title: "Last Night in Soho",
+    author: "Wright and Krysty Wilson-Cairns",
+    URL: "https://www.youtube.com/watch?v=AcVnFrxjPjI",
+  },
+  {
+    id: 5,
+    title: "Dune",
+    author: "Frank Herbert",
+    URL: "https://www.youtube.com/watch?v=8g18jFHCLXk",
+  },
+];
+
+export default movies;
