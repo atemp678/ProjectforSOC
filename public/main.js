2bf36e1a0af8782ed522f3ebc3dbd455c608ec35
Murray2015 <murrayhoggett@gmail.com>
2022-01-05 21:02:20 +0000

    I've added code to add a list of elements to the page. This is obviously both simple and buggy (e.g. try clicking the button twice!) but gives you an idea of how you can do things.

--- public/main.js
+++ public/main.js
@@ -1,11 +1,17 @@
 const getMovieButton = document.getElementById("getMovieButton"); 
 const movieSearch = document.getElementById("movieSearch");
+const resultsList = document.getElementById("resultsList"); 
 
 getMovieButton.addEventListener("click", () => {
     fetch(`http://localhost:3000/movies?search=${movieSearch.value}`)
     .then(res => res.json())
     .then(result => {
-        console.log("Result", result);
+        result.payload.forEach(element => {
+            const li = document.createElement("li"); 
+            li.innerText = element.title; 
+            resultsList.appendChild(li);
+        });
+
     })
 }); 
 
